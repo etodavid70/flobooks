11652023074aaa4fc9bca2c8992149efab6a6483
eto david <etodavid70@gmail.com>
2024-11-09 05:21:04 +0100

    decimal

--- sales/serializers.py
+++ sales/serializers.py
@@ -69,8 +69,8 @@ class PurchaseSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # Extract and remove 'amount_paid_in_cash' and 'amount_paid_to_bank' from validated_data
-        amount_paid_in_cash = validated_data.pop('amount_paid_in_cash', 0)
-        amount_paid_to_bank = validated_data.pop('amount_paid_to_bank', 0)
+        amount_paid_in_cash = Decimal(validated_data.pop('amount_paid_in_cash', 0))
+        amount_paid_to_bank = Decimal(validated_data.pop('amount_paid_to_bank', 0))
         
         # Calculate the total amount
         validated_data['amount'] = amount_paid_in_cash + amount_paid_to_bank
